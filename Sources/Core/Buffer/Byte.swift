bc6b38bf34018f27e7c6842be4fc5410ad42a32c
David Ask <david@formbound.com>
2017-04-18 17:18:27 +0200

    Foundation.Data now conforming to DataConvertible

--- Sources/Core/Buffer/Byte.swift
+++ Sources/Core/Buffer/Byte.swift
@@ -123,30 +123,3 @@ extension UnsafeMutableBufferPointer {
     }
 }
 
-public protocol DataInitializable {
-    init(bytes: [Byte]) throws
-}
-
-public protocol DataRepresentable {
-    var bytes: [Byte] { get }
-}
-
-public protocol DataConvertible : DataInitializable, DataRepresentable {}
-
-public enum BufferConversionError: Error {
-    case invalidString
-}
-
-extension String : DataConvertible {
-    public init(bytes: [Byte]) throws {
-        guard let string = String(bytes: bytes, encoding: .utf8) else {
-            throw BufferConversionError.invalidString
-        }
-        self = string
-    }
-
-    public var bytes: [Byte] {
-        return [Byte](self)
-    }
-}
-
