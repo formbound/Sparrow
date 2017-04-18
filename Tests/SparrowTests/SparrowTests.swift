c4994506d40019745c533937a056e8163a18405d
David Ask <david@formbound.com>
2017-04-18 17:28:33 +0200

    Updates

---
+++ Tests/SparrowTests/SparrowTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import Sparrow
+
+public class SparrowTests : XCTestCase {
+
+    func testServer() throws {
+        let log = LogMiddleware()
+
+        let router = BasicRouter { route in
+            route.get("/") { request in
+                return Response(body: "Hello, world!")
+            }
+        }
+
+        let server = try Server(port: 8080, middleware: [log], responder: router)
+        try server.start()
+    }
+}
+
+extension SparrowTests {
+    public static var allTests: [(String, (SparrowTests) -> () throws -> Void)] {
+        return []
+    }
+}
