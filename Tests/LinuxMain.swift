b9af9f28d1ecad145b1adbaf0deee022277143de
David Ask <david@formbound.com>
2017-04-23 10:51:00 +0200

    Added some tests

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,17 @@
+#if os(Linux)
+
+import XCTest
+
+@testable import IPTests
+@testable import TCPTests
+@testable import OpenSSLTests
+@testable import POSIXTests
+
+XCTMain([
+    testCase(IPTests.allTests),
+    testCase(TCPTests.allTests),
+    testCase(POSIXTests.allTests),
+])
+
+
+#endif
