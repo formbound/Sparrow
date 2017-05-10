2acc8a888e2966267b2214768b3661cfd17a8aec
Paulo Faria <paulo.faria.rl@gmail.com>
2017-05-10 15:15:45 -0300

    move C to Source

--- Package.swift
+++ Package.swift
@@ -3,20 +3,21 @@ import PackageDescription
 let package = Package(
     name: "Sparrow",
     targets: [
-        Target(name: "Core"),
-        Target(name: "POSIX"),
-        Target(name: "Networking", dependencies: ["Core", "POSIX"]),
-        Target(name: "HTTP", dependencies: ["Networking"]),
+        Target(name: "CDNS"),
+        Target(name: "CPOSIX"),
+        Target(name: "CHTTPParser"),
+        Target(name: "CYAJL"),
+        
+        Target(name: "POSIX", dependencies: ["CPOSIX"]),
+        Target(name: "Core", dependencies: ["CYAJL"]),
+        Target(name: "Networking", dependencies: ["CDNS", "Core", "POSIX"]),
+        Target(name: "HTTP", dependencies: ["CHTTPParser", "Networking"]),
         Target(name: "Crest", dependencies: ["HTTP"]),
         Target(name: "Sparrow", dependencies: ["HTTP"]),
         Target(name: "Example", dependencies: ["Sparrow"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/formbound/Venice.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/CDNS.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/CPOSIX.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0),
         .Package(url: "https://github.com/formbound/Powerline.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/CYAJL.git", majorVersion: 0)
     ]
 )
