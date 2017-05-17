e2da08fb41c2a6328092fd8fa7a78a7c883831a4
Paulo Faria <paulo.faria.rl@gmail.com>
2017-05-17 11:17:14 -0300

    add sparrow header to server

--- Sources/Sparrow/Server/Server.swift
+++ Sources/Sparrow/Server/Server.swift
@@ -5,26 +5,41 @@ import HTTP
 extension Server {
     /// Creates a new HTTP server
     public convenience init(
+        router: Router,
+        logger: Logger = defaultLogger,
+        header: String = defaultHeader,
         parserBufferSize: Int = 4096,
         serializerBufferSize: Int = 4096,
         parseTimeout: Duration = 5.minutes,
         serializeTimeout: Duration = 5.minutes,
-        closeConnectionTimeout: Duration = 1.minute,
-        logAppenders: [LogAppender] = [defaultAppender],
-        router: Router
+        closeConnectionTimeout: Duration = 1.minute
     ) {
         self.init(
+            logger: logger,
+            header: header,
             parserBufferSize: parserBufferSize,
             serializerBufferSize: serializerBufferSize,
             parseTimeout: parseTimeout,
             serializeTimeout: serializeTimeout,
             closeConnectionTimeout: closeConnectionTimeout,
-            logAppenders: logAppenders,
-            respond: router.respond(to:)
+            respond: router.respond
         )
     }
     
-    private static var defaultAppender: LogAppender {
-        return StandardOutputAppender(name: "HTTP server", levels: [.error, .info])
+    private static var defaultLogger: Logger {
+        let appender = StandardOutputAppender(name: "HTTP server", levels: [.error, .info])
+        return Logger(name: "HTTP server", appenders: [appender])
+    }
+    
+    private static var defaultHeader: String {
+        var header = "\n"
+        header += "      _____                                          \n"
+        header += "     / ___/ ____   ____ _ _____ _____ ____  _      __\n"
+        header += "     \\__ \\ / __ \\ / __ `// ___// ___// __ \\| | /| / /\n"
+        header += "    ___/ // /_/ // /_/ // /   / /   / /_/ /| |/ |/ / \n"
+        header += "   /____// .___/ \\__,_//_/   /_/    \\____/ |__/|__/  \n"
+        header += "        /_/                                          \n"
+        header += "-------------------------------------------------------\n"
+        return header
     }
 }
